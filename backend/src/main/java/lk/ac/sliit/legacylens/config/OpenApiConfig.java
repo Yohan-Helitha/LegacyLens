9890808ccf543857ea0079745ea0c550b902f096
LakniR23 <shashiprabhalakni@gmail.com>
2026-09-07 14:13:32 +0530

    feat: add OpenAPI/Swagger configuration

---
+++ backend/src/main/java/lk/ac/sliit/legacylens/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package lk.ac.sliit.legacylens.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "LegacyLens API", version = "1.0", description = "API documentation for LegacyLens backend"),
+    security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class OpenApiConfig {
+}
+
